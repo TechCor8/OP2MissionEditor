3bee2e8f14f693ce840d58317b54d0ca2a90b261
TechCor8 <48194787+TechCor8@users.noreply.github.com>
2020-01-26 02:00:40 -0500

    Menus will activate on mouse over if one is already active

---
+++ Assets/Scripts/UserInterface/MouseOverToggle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace OP2MissionEditor.UserInterface
+{
+	/// <summary>
+	/// Activates Toggle button on mouse over if the ToggleGroup has active toggles.
+	/// </summary>
+	[RequireComponent(typeof(Toggle))]
+	public class MouseOverToggle : MonoBehaviour, IPointerEnterHandler
+	{
+		public void OnPointerEnter(PointerEventData eventData)
+		{
+			Toggle toggle = GetComponent<Toggle>();
+
+			if (toggle.group != null)
+			{
+				if (toggle.group.AnyTogglesOn())
+					toggle.isOn = true;
+			}
+		}
+	}
+}
