6f08cc9ceab31d80023e2658106c24f115c604f6
TechCor8 <48194787+TechCor8@users.noreply.github.com>
2020-04-05 07:52:28 -0400

    Added confirm and info dialogs

---
+++ Assets/Scripts/Dialogs/Generic/ConfirmDialog.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace OP2MissionEditor.Dialogs.Generic
+{
+	public class ConfirmDialog : MonoBehaviour
+	{
+		[SerializeField] private Text m_txtTitle						= default;
+		[SerializeField] private Text m_txtBody							= default;
+
+		[SerializeField] private Text m_txtSubmit						= default;
+		[SerializeField] private Text m_txtCancel						= default;
+
+		public delegate void OnDialogCallback(bool didConfirm);
+
+		protected OnDialogCallback m_OnDialogClosedCB;
+
+
+		public TextAnchor bodyAlignment
+		{
+			get { return m_txtBody.alignment;	}
+			set { m_txtBody.alignment = value;	}
+		}
+
+
+		// Use this for initialization
+		public void Initialize(OnDialogCallback cb, string title, string body, string submit="OK", string cancel="Cancel")
+		{
+			m_txtTitle.text = title;
+			m_txtBody.text = body;
+
+			m_txtSubmit.text = submit;
+			m_txtCancel.text = cancel;
+			
+			m_OnDialogClosedCB = cb ?? throw new System.ArgumentNullException("cb");
+		}
+
+		public void OnClick_Submit()
+		{
+			m_OnDialogClosedCB?.Invoke(true);
+
+			Destroy(gameObject);
+		}
+
+		public void OnClick_Cancel()
+		{
+			m_OnDialogClosedCB?.Invoke(false);
+
+			Destroy(gameObject);
+		}
+
+		public static ConfirmDialog Create(OnDialogCallback cb, string title, string body, string submit="OK", string cancel="Cancel")
+		{
+			GameObject goDlg = Instantiate(Resources.Load<GameObject>("Dialogs/Generic/CanvasConfirmDialog"));
+			ConfirmDialog dlg = goDlg.GetComponent<ConfirmDialog>();
+
+			dlg.Initialize(cb, title, body, submit, cancel);
+
+			return dlg;
+		}
+	}
+}
---
+++ Assets/Scripts/Dialogs/Generic/InfoDialog.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace OP2MissionEditor.Dialogs.Generic
+{
+	public class InfoDialog : MonoBehaviour
+	{
+		[SerializeField] private Text m_txtTitle						= default;
+		[SerializeField] private Text m_txtBody							= default;
+
+		public delegate void OnDialogCallback();
+
+		protected OnDialogCallback m_OnDialogClosedCB;
+
+
+		public TextAnchor bodyAlignment
+		{
+			get { return m_txtBody.alignment;	}
+			set { m_txtBody.alignment = value;	}
+		}
+
+
+		// Use this for initialization
+		public void Initialize(string title, string body, OnDialogCallback cb=null)
+		{
+			m_txtTitle.text = title;
+			m_txtBody.text = body;
+			
+			m_OnDialogClosedCB = cb;
+		}
+
+		public void OnClick_Close()
+		{
+			m_OnDialogClosedCB?.Invoke();
+
+			Destroy(gameObject);
+		}
+
+		// Update is called once per frame
+		private void Update()
+		{
+			// Trigger close on enter key
+			if (Input.GetKeyDown(KeyCode.Return))
+				OnClick_Close();
+		}
+
+		public static InfoDialog Create(string title, string body, OnDialogCallback cb=null)
+		{
+			GameObject goDlg = Instantiate(Resources.Load<GameObject>("Dialogs/Generic/CanvasInfoDialog"));
+			InfoDialog dlg = goDlg.GetComponent<InfoDialog>();
+
+			dlg.Initialize(title, body, cb);
+
+			return dlg;
+		}
+	}
+}
